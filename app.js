f8bceaabd8c9cda5eea6b00ed7e35ee3cbb834a7
Michel Capela <michel.capela@outlook.com>
2024-10-08 17:11:40 -0300

    ajuste de PORT e script

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
