24db41f92ae671df44a6f6eaa21128df757e66e5
Michel Capela <michel.capela@outlook.com>
2024-10-12 15:27:37 -0300

    removing unused folders and files

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
-const path = require('path')
 
 const database = require('./config')
 const taskRoutes = require('./routes/taskRoutes');
@@ -10,18 +9,12 @@ const app = express();
 
 (async () => { await database.sync(); })();
 
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/tasks', taskRoutes);
 app.use('/users', userRoutes);
 
-/*app.get('/', (req, res) => {
-  res.render('index')
-})*/
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
